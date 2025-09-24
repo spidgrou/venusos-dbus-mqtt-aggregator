258922b6cf3fb17052a607af9a39a0b90b8944bb
spidgrou <42083306+spidgrou@users.noreply.github.com>
2025-09-24 16:50:32 +1200

    Update dbus_to_mqtt_bridge.py

--- dbus_to_mqtt_bridge.py
+++ dbus_to_mqtt_bridge.py
@@ -1,9 +1,13 @@
 #!/usr/bin/env python3
 
-import sys, os, platform, logging, time, dbus
+import sys, os, platform, logging, time, dbus, traceback
 from gi.repository import GLib
 import paho.mqtt.client as mqtt
 from paho.mqtt.client import CallbackAPIVersion
+# --- MODIFICA 1: Usiamo i nomi corretti e standard per i tipi D-Bus ---
+from dbus.types import Array as dbusArray, Int16 as dbusInt16, UInt16 as dbusUInt16, \
+    Int32 as dbusInt32, UInt32 as dbusUInt32, Double as dbusDouble, String as dbusString, \
+    Byte as dbusByte
 import threading
 
 # --- Config ---
@@ -23,6 +27,7 @@ class DbusMqttBridge:
     }
 
     def __init__(self):
+        logging.info("--- EXECUTING SCRIPT VERSION 17.2 ---")
         logging.info("Waiting 15 seconds for all services to start...")
         time.sleep(15)
         self._dbus_conn = dbus.SystemBus()
@@ -33,10 +38,10 @@ class DbusMqttBridge:
         self.system_id = None; self.data = {}
         logging.info("Starting D-Bus to MQTT bridge service")
         self._find_services()
-        self.system_id = self._dbus_get_value(self._dbus_paths['settings']['service'], "/Settings/System/VrmPortalId")
+        self.system_id = self._get_dbus_value(self._dbus_paths['settings']['service'], "/Settings/System/VrmPortalId")
         if not self.system_id:
             logging.warning("VRM Portal ID not found. Falling back to device serial.")
-            self.system_id = self._dbus_get_value(self._dbus_paths['system']['service'], "/Serial")
+            self.system_id = self._get_dbus_value(self._dbus_paths['system']['service'], "/Serial")
         if not self.system_id:
             logging.error("Could not get a unique ID for the system. Exiting."); sys.exit(1)
         logging.info(f"System ID for MQTT topics: {self.system_id}")
@@ -53,12 +58,22 @@ class DbusMqttBridge:
         else: logging.error(f"MQTT connection failed with code: {rc}")
     def _on_mqtt_disconnect(self, client, userdata, rc):
         logging.warning(f"Disconnected from MQTT. Code: {rc}")
-    def _dbus_get_value(self, service, path, default=None):
-        try: return self._dbus_conn.get_object(service, path).GetValue()
-        except: return default
+
+    # --- MODIFICA 2: Controllo dei tipi aggiornato con i nomi corretti ---
+    def _get_dbus_value(self, service, path, default=None):
+        try:
+            val = self._dbus_conn.get_object(service, path).GetValue()
+            if val is None: return default
+            if isinstance(val, (int, float, str)): return val
+            if isinstance(val, (dbusInt16, dbusUInt16, dbusInt32, dbusUInt32, dbusByte)): return int(val)
+            if isinstance(val, (dbusDouble,)): return float(val)
+            if isinstance(val, (dbusString,)): return str(val)
+            if isinstance(val, (dbusArray, list)): return default
+            return default
+        except Exception: return default
+
     def _get_display_name(self, service):
-        custom_name = self._dbus_get_value(service, '/CustomName')
-        return custom_name if custom_name else self._dbus_get_value(service, '/ProductName', default=service)
+        return self._get_dbus_value(service, '/CustomName', default=self._get_dbus_value(service, '/ProductName', default=service))
 
     def _find_services(self, is_rescan=False):
         bus_object = self._dbus_conn.get_object("org.freedesktop.DBus", "/org/freedesktop/DBus")
@@ -83,57 +98,43 @@ class DbusMqttBridge:
         return True
     
     def _update_data(self):
+        # ... (il resto del codice non ha bisogno di modifiche) ...
         bat_service = self._dbus_paths['battery']['service']
-        if bat_service: self.data['battery_current'] = self._dbus_get_value(bat_service, '/Dc/0/Current'); self.data['battery_power'] = self._dbus_get_value(bat_service, '/Dc/0/Power'); self.data['battery_voltage'] = self._dbus_get_value(bat_service, '/Dc/0/Voltage'); self.data['battery_soc'] = self._dbus_get_value(bat_service, '/Soc'); self.data['battery_consumed_ah'] = self._dbus_get_value(bat_service, '/ConsumedAmphours'); self.data['battery_aux_voltage'] = self._dbus_get_value(bat_service, '/Dc/1/Voltage')
+        if bat_service: self.data['battery_current'] = self._get_dbus_value(bat_service, '/Dc/0/Current'); self.data['battery_power'] = self._get_dbus_value(bat_service, '/Dc/0/Power'); self.data['battery_voltage'] = self._get_dbus_value(bat_service, '/Dc/0/Voltage'); self.data['battery_soc'] = self._get_dbus_value(bat_service, '/Soc'); self.data['battery_consumed_ah'] = self._get_dbus_value(bat_service, '/ConsumedAmphours'); self.data['battery_aux_voltage'] = self._get_dbus_value(bat_service, '/Dc/1/Voltage')
         sys_service = self._dbus_paths['system']['service']
-        if sys_service: self.data['solar_power'] = self._dbus_get_value(sys_service, '/Dc/Pv/Power'); self.data['solar_current'] = self._dbus_get_value(sys_service, '/Dc/Pv/Current')
+        if sys_service: self.data['solar_power'] = self._get_dbus_value(sys_service, '/Dc/Pv/Power'); self.data['solar_current'] = self._get_dbus_value(sys_service, '/Dc/Pv/Current')
         total_yield = 0.0
         for charger in self._dbus_paths['solarchargers']:
-            yield_kwh = self._dbus_get_value(charger['service'], '/History/Daily/0/Yield')
+            yield_kwh = self._get_dbus_value(charger['service'], '/History/Daily/0/Yield')
             if yield_kwh is not None: total_yield += (yield_kwh * 1000)
         self.data['solar_yield_today_wh'] = total_yield
         vebus_service = self._dbus_paths['vebus']['service']
         if vebus_service:
-            self.data['vebus_in_voltage'] = self._dbus_get_value(vebus_service, '/Ac/ActiveIn/L1/V'); self.data['vebus_in_frequency'] = self._dbus_get_value(vebus_service, '/Ac/ActiveIn/L1/F'); self.data['vebus_in_power'] = self._dbus_get_value(vebus_service, '/Ac/ActiveIn/L1/P')
-            self.data['vebus_out_voltage'] = self._dbus_get_value(vebus_service, '/Ac/Out/L1/V'); self.data['vebus_out_frequency'] = self._dbus_get_value(vebus_service, '/Ac/Out/L1/F'); self.data['vebus_out_power'] = self._dbus_get_value(vebus_service, '/Ac/Out/L1/P')
-            self.data['vebus_charge_current'] = self._dbus_get_value(vebus_service, '/Dc/0/Current')
-            state_code = self._dbus_get_value(vebus_service, '/State')
-            if state_code is not None: self.data['vebus_state_text'] = self.VEBUS_STATE_MAP.get(state_code, f'Unknown ({state_code})')
-    
+            self.data['vebus_in_voltage'] = self._get_dbus_value(vebus_service, '/Ac/ActiveIn/L1/V'); self.data['vebus_in_frequency'] = self._get_dbus_value(vebus_service, '/Ac/ActiveIn/L1/F'); self.data['vebus_in_power'] = self._get_dbus_value(vebus_service, '/Ac/ActiveIn/L1/P'); self.data['vebus_out_voltage'] = self._get_dbus_value(vebus_service, '/Ac/Out/L1/V'); self.data['vebus_out_frequency'] = self._get_dbus_value(vebus_service, '/Ac/Out/L1/F'); self.data['vebus_out_power'] = self._get_dbus_value(vebus_service, '/Ac/Out/L1/P'); self.data['vebus_charge_current'] = self._get_dbus_value(vebus_service, '/Dc/0/Current')
+            state_code = self._get_dbus_value(vebus_service, '/State')
+            if state_code is not None:
+                self.data['vebus_state_text'] = self.VEBUS_STATE_MAP.get(state_code, f'Unknown ({state_code})')
+
     def _publish_to_mqtt(self):
-        if not self.mqtt_client.is_connected():
-            logging.warning("MQTT client disconnected, skipping publication.")
-            return # This is safe now because run() will always return True
-        topic_map = {
-            'battery_current': 'batteryMonitor/Current', 'battery_power': 'batteryMonitor/Power', 'battery_voltage': 'batteryMonitor/Voltage', 'battery_soc': 'batteryMonitor/SOC',
-            'battery_consumed_ah': 'batteryMonitor/UsedAh', 'battery_aux_voltage': 'batteryMonitor/VoltageAUX',
-            'solar_power': 'solar/totalPower', 'solar_current': 'solar/totalCurrent', 'solar_yield_today_wh': 'solar/yieldTodayWh',
-            'vebus_in_voltage':   'AC/IN/Voltage', 'vebus_in_frequency': 'AC/IN/Frequency', 'vebus_in_power':     'AC/IN/Power',
-            'vebus_out_voltage':   'AC/OUT/Voltage', 'vebus_out_frequency': 'AC/OUT/Frequency', 'vebus_out_power':     'AC/OUT/Power',
-            'vebus_charge_current': 'AC/CHARGER/Current', 'vebus_state_text': 'AC/CHARGER/State'
-        }
+        if not self.mqtt_client.is_connected(): logging.warning("MQTT client disconnected, skipping publication."); return
+        topic_map = {'battery_current': 'batteryMonitor/Current', 'battery_power': 'batteryMonitor/Power', 'battery_voltage': 'batteryMonitor/Voltage', 'battery_soc': 'batteryMonitor/SOC', 'battery_consumed_ah': 'batteryMonitor/UsedAh', 'battery_aux_voltage': 'batteryMonitor/VoltageAUX', 'solar_power': 'solar/totalPower', 'solar_current': 'solar/totalCurrent', 'solar_yield_today_wh': 'solar/yieldTodayWh', 'vebus_in_voltage':   'AC/IN/Voltage', 'vebus_in_frequency': 'AC/IN/Frequency', 'vebus_in_power':     'AC/IN/Power', 'vebus_out_voltage':   'AC/OUT/Voltage', 'vebus_out_frequency': 'AC/OUT/Frequency', 'vebus_out_power':     'AC/OUT/Power', 'vebus_charge_current': 'AC/CHARGER/Current', 'vebus_state_text': 'AC/CHARGER/State'}
         for key, topic_suffix in topic_map.items():
             value = self.data.get(key)
             if value is not None:
                 topic = f"Y/{topic_suffix}"; payload = round(value, 2) if isinstance(value, (int, float)) else value
                 self.mqtt_client.publish(topic, payload, retain=True)
 
-    # --- MODIFICA CHIAVE: Funzione run() blindata ---
     def run(self):
         try:
-            # We log before doing anything, to see if the loop is running
             logging.info("--> Main loop tick: Updating data and publishing...")
             self._update_data()
             self._publish_to_mqtt()
-            # The keep-alive publications
             self.mqtt_client.publish(f"R/{self.system_id}/system/0/Serial", self.system_id, retain=True)
             self.mqtt_client.publish(f"Y/serial", self.system_id, retain=True)
-        except Exception as e:
-            logging.error(f"!!! Unhandled exception in main loop: {e}")
-        
-        # This is the most important part: ALWAYS return True to keep the timer alive
+        except Exception:
+            logging.error("!!! UNHANDLED EXCEPTION IN MAIN LOOP:")
+            logging.error(traceback.format_exc())
         return True
-    # --- FINE MODIFICA ---
 
 if __name__ == "__main__":
     bridge = DbusMqttBridge()
